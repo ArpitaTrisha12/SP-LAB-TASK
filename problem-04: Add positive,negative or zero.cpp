01a2736a799d3035af051b2f9b3df12d87fd8d05
ArpitaTrisha12 <arpitabhadratrisha@gmail.com>
2024-09-24 14:23:46 +0600

    Create problem-04: Add positive,negative or zero.cpp

---
+++ problem-04: Add positive,negative or zero.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int n;
+scanf("%d",&n);
+if(n>0)
+printf("%d is an positive number",n);
+else if (n<0)
+ printf("%d is an negative number",n);
+else
+printf("Zero");
+return 0;
+
+}
