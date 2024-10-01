3c3bb4a29191fbe4c6b43fa8b2b1338355a55540
ArpitaTrisha12 <arpitabhadratrisha@gmail.com>
2024-10-02 01:05:06 +0600

    Create problem -9: Add Find Maximum Element in Array.cpp

---
+++ problem -9: Add Find Maximum Element in Array.cpp
@@ -0,0 +1,15 @@
+int main()
+{
+int a[]={5,2,1,8,0,7,9,2,10,20};
+
+int maxElement=0;
+
+for(int i=0;i<10;i++){
+if(a[i]>maxElement)
+{
+maxElement=a[i];
+}
+printf("max element:%d",maxElement);
+}
+
+Return 0; }
